6ff5d45b4c4aa26664d8243f1a5ddc75d3eee360
김태히 <k.tehi@gimtaehiui-MacBookPro.local>
2023-05-12 20:23:05 -0700

    save

--- express-back-end/db/queries/events.js
+++ express-back-end/db/queries/events.js
@@ -1,12 +1,47 @@
 const db = require('../connection');
 
-const getAllEvents = () => {
-  const queryString = `SELECT * FROM events`;
-  return db.query(queryString)
-    .then(result => {
-      return result.rows
-    })
-    .catch((err) => console.log(err.message))
-}
+// CRUD - Create, Read, Update, Delete
+const create = (newEvent) => {
+  const { name, description, host_id } = newEvent;
+  return db
+    .query(
+      'INSERT INTO events (name, description, host_id) VALUES ($1, $2, $3) RETURNING *;',
+      [name, description, host_id]
+    )
+    .then((data) => data.rows[0]);
+};
 
-module.exports = getAllEvents;
+const getAll = () => {
+  return db.query('SELECT * FROM events;').then((data) => data.rows);
+};
+
+const getById = (id) => {
+  return db
+    .query('SELECT * FROM events WHERE id = $1;', [id])
+    .then((data) => data.rows[0]);
+};
+
+const getByHostId = (host_id) => {
+  return db
+    .query('SELECT * FROM events WHERE host_id = $1;', [host_id])
+    .then((data) => data.rows);
+};
+
+const update = (updatedEvent) => {
+  const { id, name, description } = updatedEvent;
+  return db
+    .query('UPDATE events SET (name = $1 AND description = $2) WHERE id = $3 RETURNING *;', [
+      name, 
+      description,
+      id,
+    ])
+    .then((data) => data.rows[0]);
+};
+
+const remove = (id) => {
+  return db
+    .query('DELETE FROM events WHERE id = $1;', [id])
+    .then((data) => data.rows);
+};
+
+module.exports = { create, getAll, getById, getByHostId, update, remove };
--- express-back-end/routes/api-event.js
+++ express-back-end/routes/api-event.js
@@ -1,12 +1,136 @@
 const express = require('express');
 const router = express.Router();
 
-const getAllEvents = require('../db/queries/events')
+const eventQueries= require('../db/queries/events');
 
-// CRUD api/users
-router.get('/events', (req, res) => {
-  getAllEvents()
+///// CRUD
+//// CREATE
+router.post('/', (req, res) => {
+  const { host_id } = req.session;
+  if (!host_id) {
+    return res.status(401).json({ message: 'User is not logged in' });
+  }
+
+  const { name, description, agenda } = req.body;
+  if (!name || !description || !agenda) {
+    return res
+      .status(403)
+      .render('error', { message: 'Provide event name, description, and agenda to new event!' });
+  }
+
+  const newEvent = { host_id, name, description, agenda };
+  eventQueries
+    .create(newEvent)
+    .then((event) => {
+      res.status(201).json({ message: 'Event created!', note });
+      res.redirect('/');
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .render('error', { message: `Error registering user: ${err.message}` });
+    });
+});
+
+//// READ
+router.get('/', (req, res) => {
+  eventQueries
+  .getAll()
   .then(events => res.send(events))
+});
+
+//// Read one - GET
+router.get('/:id', (req, res) => {
+  eventQueries
+    .getById(req.params.id)
+    .then((event) => {
+      if (!event) {
+        return res.status(400).json({ message: 'Event not found!' });
+      }
+
+      res.status(201).json({ message: 'Here is your event!', event });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: 'Error reading event', error: err.message });
+    });
+});
+
+//// Update - POST
+router.post('/:id/edit', (req, res) => {
+  const { host_id } = req.session;
+  if (!host_id) {
+    return res.status(401).json({ message: 'User is not logged in' });
+  }
+
+  const { content } = req.body;
+  if (!content) {
+    return res
+      .status(400)
+      .json({ message: 'All properties must be provided to update a note' });
+  }
+
+  const { id } = req.params;
+  notesQueries
+    .getById(id)
+    .then((note) => {
+      if (!note) {
+        return res.status(404).json({ message: 'Note not found!' });
+      }
+
+      console.log(note);
+      const noteBelongsToUser = note.user_id === user_id;
+      if (!noteBelongsToUser) {
+        return res
+          .status(401)
+          .json({ message: 'Note does not belongs to you!' });
+      }
+
+      return notesQueries.update({ id, content });
+    })
+    .then((updatedNote) => {
+      res.status(201).json({ message: 'Note updated!', note: updatedNote });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: 'Error updating note', error: err.message });
+    });
+});
+
+// Delete - POST
+router.post('/:id/delete', (req, res) => {
+  const { user_id } = req.session;
+  if (!user_id) {
+    return res.status(401).json({ message: 'User is not logged in' });
+  }
+
+  const { id } = req.params;
+  notesQueries
+    .getById(id)
+    .then((note) => {
+      if (!note) {
+        return res.status(404).json({ message: 'Note not found!' });
+      }
+
+      const noteBelongsToUser = note.user_id === user_id;
+      if (!noteBelongsToUser) {
+        return res
+          .status(401)
+          .json({ message: 'Note does not belongs to you!' });
+      }
+
+      return notesQueries.remove(id);
+    })
+    .then(() => {
+      res.status(204).json();
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: 'Error deleting note', error: err.message });
+    });
 });
 
 module.exports = router;
