362c3c48265a09117985297f59ce32ae5b457caf
Rain Zhu <rain1995zhu@gmail.com>
2023-05-14 11:03:06 -0700

    add basic event page scss styles

--- react-front-end/src/components/events/events.js
+++ react-front-end/src/components/events/events.js
@@ -13,24 +13,31 @@ export default function Events(props) {
 
   return (
     <main className="event-layout">
-      <section className="invitation">
-        <span>invitation</span>
-      </section>
-      <section className="events-info">
-      <span>events-info</span>
-      </section>
-      <section className="events-guest">
-      <span>events-guest</span>
-      </section>
-      <section className="maps-api">
-      <span>maps-api</span>
-      </section>
-      <section className="fundraisers">
-      <span>fundraisers</span>
-      </section>
-      <section className="events-wall">
-      <span>events-wall</span>
-      </section>
+
+      <div className='event-left-panel'>
+        <section className="invitation">
+          <span>invitation</span>
+        </section>
+        <section className="event-info">
+        <span>events-info</span>
+        </section>
+        <section className="event-guest">
+        <span>events-guest</span>
+        </section>
+      </div>
+
+      <div className='event-right-panel'>
+        <section className="maps-api">
+        <span>maps-api</span>
+        </section>
+        <section className="fundraisers">
+        <span>fundraisers</span>
+        </section>
+        <section className="event-wall">
+        <span>events-wall</span>
+        </section>
+      </div>
+
     </main>
   )
 }
