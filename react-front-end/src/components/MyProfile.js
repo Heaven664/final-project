f7448df1ae7c9e3c6d718eb0b8ae53acf028f8f1
tehi_kim <k.tehi21@gmail.com>
2023-05-17 05:30:18 -0700

    table wrap with tbody tag

--- react-front-end/src/components/MyProfile.js
+++ react-front-end/src/components/MyProfile.js
@@ -21,17 +21,17 @@ export default function MyProfile(props) {
   // get the user data from api
   useEffect(() => {
     axios.get(`/api/users/${state.id}`)
-    .then((res) => {
-      const user = res.data;
-      // birthday formatting: leave only YYYY-MM-DD
-      if (user.birthday) {
-        user.birthday = user.birthday.substring(0, 10); 
-      }
-      setState(user);
-    })
-    .catch(err => {
-      console.error("connect error:", err.message);
-    })
+      .then((res) => {
+        const user = res.data;
+        // birthday formatting: leave only YYYY-MM-DD
+        if (user.birthday) {
+          user.birthday = user.birthday.substring(0, 10);
+        }
+        setState(user);
+      })
+      .catch(err => {
+        console.error("connect error:", err.message);
+      })
   }, []);
 
   return (
@@ -45,26 +45,28 @@ export default function MyProfile(props) {
         </div>
         <div className="box-shadow border-radius20 background-box-color user-detail">
           <table>
-            <tr>
-              <th>Name</th>
-              <td>{state.first_name} {state.last_name}</td>
-            </tr>
-            <tr>
-              <th>Country</th>
-              <td>{state.country}</td>
-            </tr>
-            <tr>
-              <th>City</th>
-              <td>{state.city}</td>
-            </tr>
-            <tr>
-              <th>Birthday</th>
-              <td>{state.birthday}</td>
-            </tr>
-            <tr>
-              <th>About me</th>
-              <td>{state.about}</td>
-            </tr>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <td>{state.first_name} {state.last_name}</td>
+              </tr>
+              <tr>
+                <th>Country</th>
+                <td>{state.country}</td>
+              </tr>
+              <tr>
+                <th>City</th>
+                <td>{state.city}</td>
+              </tr>
+              <tr>
+                <th>Birthday</th>
+                <td>{state.birthday}</td>
+              </tr>
+              <tr>
+                <th>About me</th>
+                <td>{state.about}</td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
