0419d48caceac3d6f36eca039ac38d0a469b6d88
Rain Zhu <rain1995zhu@gmail.com>
2023-05-18 05:06:47 -0700

    update seeds

--- express-back-end/routes/api-event.js
+++ express-back-end/routes/api-event.js
@@ -3,39 +3,39 @@ const router = express.Router();
 
 const eventQueries= require('../db/queries/events');
 
-//// CREATE
-router.post('/', (req, res) => {
-  // const { host_id } = req.session;
-  // if (!host_id) {
-  //   return res.status(401).json({ message: 'User is not logged in' });
-  // }
+// //// CREATE
+// router.post('/', (req, res) => {
+//   // const { host_id } = req.session;
+//   // if (!host_id) {
+//   //   return res.status(401).json({ message: 'User is not logged in' });
+//   // }
 
-  const { name, description, agenda, host_id } = req.body; 
-  if (!name ) {
-    return res
-      .status(403)
-      .json({ message: 'Provide event name to open a new event!' });
-  }
-  if (!description) {
-    return res
-      .status(403)
-      .json({ message: 'Provide event description to open a new event!' });
-  }
-  if (!agenda) {
-    return res
-      .status(403)
-      .json({ message: 'Provide event agenda to open a new event!' });
-  }
+//   const { name, description, agenda, host_id, date, location } = req.body; 
+//   // if (!name ) {
+//   //   return res
+//   //     .status(403)
+//   //     .json({ message: 'Provide event name to open a new event!' });
+//   // }
+//   // if (!description) {
+//   //   return res
+//   //     .status(403)
+//   //     .json({ message: 'Provide event description to open a new event!' });
+//   // }
+//   // if (!agenda) {
+//   //   return res
+//   //     .status(403)
+//   //     .json({ message: 'Provide event agenda to open a new event!' });
+//   // }
 
-  eventQueries
-    .create(name, description, agenda, host_id)
-    .then((event) => res.json( event ))
-    .catch((err) => {
-      res
-        .status(500)
-        .json({ message: `Error registering event: ${err.message}` });
-    });
-});
+//   eventQueries
+//     .create(name, description, agenda, host_id, date, location)
+//     .then((event) => res.json( event ))
+//     .catch((err) => {
+//       res
+//         .status(500)
+//         .json({ message: `Error registering event: ${err.message}` });
+//     });
+// });
 
 //// READ
 router.get('/', (req, res) => {
@@ -73,11 +73,11 @@ router.post('/:id/edit', (req, res) => {
   //   return res.status(401).json({ message: 'User is not logged in' });
   // }
 
-  const { name, description, agenda } = req.body;
+  const { name, description, agenda, date, location } = req.body;
 
   const id  = req.params.id;
   eventQueries
-    .update(id, name, description, agenda)
+    .update(id, name, description, agenda, date, location )
     .then(event =>  res.json(event))
     .catch((err) => {
       res
@@ -120,4 +120,106 @@ router.post('/:id/delete', (req, res) => {
     });
 });
 
-module.exports = router;
+// module.exports = router;
+
+
+// const express = require('express');
+// const router = express.Router();
+const db = require('../db/connection');
+
+  //CRUD CREATE(POST)
+  router.post("/", (request, response) => {
+
+    const { name, description, agenda,  date, location, host } = request.body;
+
+    db.query(
+      `
+      INSERT INTO events (name, description, agenda, event_date, event_location, host_id)
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING *;
+      `,
+    [name, description, agenda, date, location, host])
+    .then(res => {
+      response.json(res.rows[0]);
+    })
+    .catch(error => response.json({Error: error, Message:"Error adding user to event."}));
+  });
+
+  // //CRUD READ(GET)
+  // router.get("/", (request, response) => {
+  //   db.query(
+  //     `
+  //     SELECT
+  //       *
+  //     FROM events;
+  //     `
+  //   )
+  //   .then(({rows:events}) => {
+  //     response.json(events);
+  //   })
+  //   .catch(error => response.json({Error: error, Message:"Error getting events."}));
+  // });
+
+  //get events info with for event
+  router.get("/host/:id", (request, response) => {
+    db.query(
+      `
+      SELECT
+        *
+      FROM events
+      WHERE host_id = $1;
+      `,
+    [Number(request.params.id)])
+    .then(res => {
+      response.json(res.rows);
+    })
+    .catch(error => response.json({Error: error, Message:"Error getting evens for this host."}));
+  });
+
+
+
+
+
+  // //CRUD UPDATE(PUT)
+  // router.put("/", (request, response) => {
+  //   const { event, user, id } = request.body;
+
+  //   db.query(
+  //     `
+  //     UPDATE event_user
+  //     SET event_id = $2, 
+  //         user_id = $3
+  //     WHERE id = $1;
+  //     `,
+  //     [id, event, user]
+  //   )
+  //   .then(res => {
+  //     response.json(res.rows);
+  //   })
+  //   .catch(error => response.json({Error: error, Message:"Error getting event_user for this event."}));
+  // });
+
+
+  // //CRUD DELETE
+  // router.delete("/", (request, response) => {
+  //   const { id } = request.body;
+  //   // if (process.env.TEST_ERROR) {
+  //   //   setTimeout(() => response.status(500).json({}), 1000);
+  //   //   return;
+  //   // }
+  //   db.query(
+  //     `
+  //     DELETE FROM event_user
+  //     WHERE id = $1;
+  //     `, 
+  //     [id]
+  //   )
+  //   .then(() => {
+  //     response.status(204).json();
+  //   })
+  //   .catch(error => response.json({Error: error, Message:"Error deleting the record."}));
+
+  // });
+
+
+ module.exports = router;
