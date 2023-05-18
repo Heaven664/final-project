5314889060afe8c9a129835127c3e0de3762b5e5
Rain Zhu <rain1995zhu@gmail.com>
2023-05-17 17:09:51 -0700

    add basic layouts for new event page

--- react-front-end/src/components/NewEvents/NewEvent.js
+++ react-front-end/src/components/NewEvents/NewEvent.js
@@ -8,6 +8,11 @@ export default function NewEvent(props) {
 
   // const [fundraiser, setFundaraiser] = useState(null);
   // const [eventGuest, setEventGuest] = useState(null);
+  const [state1, setState] = useState({
+    event_name: "",
+    event_time: "", 
+    event_agenda: ""
+  });
 
   const event_id = props.event;
   const {
@@ -20,16 +25,9 @@ export default function NewEvent(props) {
   console.log(state.fundraisers);
   console.log(props.user, state.eventsInfo.host_id);
 
-
-
-  const getFullName = function(obj) {
-    return obj.first_name + " " + obj.last_name;
-  };
-
   return (
-    <main className="event-layout">
-      <form onSubmit={""}>
-        <div className='event-left __panel'>
+      <form className="new-event-layout" onSubmit={""}>
+        <div className='-left __panel'>
 
           <section className="event-name __card box-shadow border-radius20 background-box-color user-detail">
             <label> 
@@ -48,39 +46,57 @@ export default function NewEvent(props) {
           <label> 
               Date and Time:
             <input
-              className="event-nameField"
+              className="event-timeField"
               type="datetime-local"
-              placeholder="YYYY-MM-DD hh:mm:ss TT"
               name="event_time"
               defaultValueExpression= "currentDate()"
+              onChange={(e) => setState({ ...state, event_time: e.target.value })}
             />
             </label>
           </section>
 
           <section className="event-agenda __card box-shadow border-radius20 background-box-color user-detail">
-
+          <label> 
+              Agenda: 
+            <input
+              className="event-agendaField"
+              type="text"
+              name="event_agenda"
+              onChange={(e) => setState({ ...state, event_agenda: e.target.value })}
+            />
+            </label>
+          </section>          
+          
+          <section className="event_description __card box-shadow border-radius20 background-box-color user-detail">
+          <label> 
+              Description: 
+            <input
+              className="event_descriptionField"
+              type="text"
+              name="event_description"
+              onChange={(e) => setState({ ...state, event_description: e.target.value })}
+            />
+            </label>
           </section>
 
-          <section className="manage-guest __card box-shadow border-radius20 background-box-color user-detail">
 
-          </section>
         </div>
 
-        <div className='event-right __panel'>
-          <section className="maps-api __card box-shadow border-radius20 background-box-color user-detail">
+        <div className=' -right __panel'>
 
+          <section className="manage-guest __card box-shadow border-radius20 background-box-color user-detail">
+            <span>Manage Guests</span>
+          </section>
+          
+          <section className="manage-fundraiser __card box-shadow border-radius20 background-box-color user-detail">
+            <span>Manage Fundraisers</span>
           </section>
 
-          <section className="maps-api __card box-shadow border-radius20 background-box-color user-detail">
-
-          </section>
-
-          <section className="event-wall __card  box-shadow border-radius20 background-box-color user-detail">
+          <section className="event-create __card  box-shadow border-radius20 background-box-color user-detail">
             <button onClick={""} className="background-point-color btn-style"> Create </button>
           </section>
 
         </div>
       </form>
-    </main>
   );
 }
