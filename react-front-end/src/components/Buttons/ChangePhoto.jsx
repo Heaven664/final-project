d9074a2a0f5c0bb5cc1832cc01bfc09485ab3846
Omar Hamid <omarhamid664@gmail.com>
2023-05-18 20:08:51 -0600

    Send new profile photo from client side to server

--- react-front-end/src/components/Buttons/ChangePhoto.jsx
+++ react-front-end/src/components/Buttons/ChangePhoto.jsx
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
 import "./ChangePhoto.scss";
+import axios from "axios";
 
 export default function ProfileButton() {
   const [selectedImage, setSelectedImage] = useState(null);
 
-  const handleFileChange = () => {
-    console.log("loaded file");
-    // setSelectedImage(event.target.files[0]);
+  const handleFileChange = (e) => {
+    setSelectedImage(e.target.files[0]);
   };
 
   const handleUpload = (e) => {
     e.preventDefault();
-    console.log("save file");
-    // const fromData = new FormData();
+    const formData = new FormData();
+    formData.append("image", selectedImage);
+    // console.log(selectedImage)
+    // console.log(formData);
+    axios.post("api/update-photo").then((res) => console.log(res.data));
   };
 
   return (
     <form id="change-photo-container" className="btn" onSubmit={handleUpload}>
-      <input type="file"
+      <input
+        type="file"
         className={"background-point-color btn-style change-photo"}
         onChange={handleFileChange}
-      >
-      </input>
+      ></input>
       <button
         className={
           "background-point-color btn-style save-photo background-primary-color"
