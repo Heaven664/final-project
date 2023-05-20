761ff9e37f344be8b69965b5a5c1a067c611ab60
Rain Zhu <rain1995zhu@gmail.com>
2023-05-20 01:07:53 -0700

    improve manage fundraiser structure and add setup phase

---
+++ react-front-end/src/components/NewEvents/ManageFundraisers/Setup.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import "./styles.scss";
+
+export default function SetupFundraiser(props) {
+
+
+  const [error, setError] = useState("");
+
+  const [state, setState] = useState({
+    title: "",
+    target_amount: ""
+  });
+
+
+  const reset = () => { setStudent({title: "",target_amount: ""})};
+
+  const cancel = () => { reset(); props.onCancel(); };
+
+  function validate() {
+    if (state.title === "") {
+      setError("Pleae give your wish a name!");
+      return;
+    }
+    if (state.target_amount === null) {
+      setError("Please set a target!");
+      return;
+    }
+    props.onSave();
+    setError("");
+  }
+
+  return (
+    <main className="">
+      <section className="">
+        <form autoComplete="off" onSubmit={(event)=>{event.preventDefault();}}>
+        <label>
+                  Make a Wish:<br/>
+                  <span id="fundraiser-wish-comment">(It could come true!)</span>
+                  <textarea
+                    className="fundraiser-wishField"
+                    type="text"
+                    name="fundraiser-wish"
+                    onChange={(e) => setState({ ...state, title: e.target.value })}
+                  />
+                </label>
+
+                <label>
+                  Target:<br/>
+                  <span id="fundraiser-target-comment">{""}</span>
+                  <textarea
+                    className="fundraiser-targetField"
+                    type="number"
+                    name="fundraiser-target"
+                    onChange={(e) => setState({ ...state, target_amount: e.target.value })}
+                  />
+                </label>
+
+        </form >
+        <section className="appointment__validation">{error}</section>
+      </section>
+
+      <section className="appointment__card-right">
+
+      <button onClick={cancel}
+        className="background-point-color btn-style"> Cancel
+        </button>
+
+        <button onClick={validate}
+        className="background-point-color btn-style"> Save
+        </button>
+
+      </section>
+    </main>
+  );
+};
