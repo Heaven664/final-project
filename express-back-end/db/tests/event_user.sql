6ba5c8ccfed28ee5737097d309ecd4756a3b48e7
Rain Zhu <rain1995zhu@gmail.com>
2023-05-20 20:36:30 -0700

    update event user api queries now able to get host info for event lis page

---
+++ express-back-end/db/tests/event_user.sql
@@ -0,0 +1,47 @@
+
+-- SELECT 
+--   e.id AS event_id,
+--   e.name AS event_name,
+--   e.description,
+--   e.agenda,
+--   e.event_date,
+--   e.event_location,
+--   h.first_name AS host_first_name,
+--   h.last_name AS host_last_name,
+--   h.photo AS host_photo,
+--   u.first_name AS user_first_name,
+--   u.last_name AS user_last_name,
+--   u.photo AS user_photo
+-- FROM 
+--   events AS e
+-- LEFT JOIN 
+--   users AS h ON e.host_id = h.id
+-- INNER JOIN (
+--   SELECT 
+--     eu.event_id
+--   FROM 
+--     event_user AS eu
+--   WHERE 
+--     eu.user_id = 1
+-- ) AS subquery ON e.id = subquery.event_id
+-- LEFT JOIN 
+--   users AS u ON u.id = 1
+-- WHERE 
+--   e.host_id <> 1
+-- ORDER BY e.event_date ASC;
+
+
+-- SELECT 
+--   e.id AS event_id,
+--   e.name AS event_name,
+--   e.event_date AS event_date,
+--   e.event_location AS event_location,
+--   h.first_name AS host_first_name,
+--   h.last_name AS host_last_name,
+--   h.photo AS host_photo
+-- FROM 
+--   events AS e
+-- LEFT JOIN 
+--   users AS h ON e.host_id = h.id
+-- WHERE 
+--   e.host_id = 1;
