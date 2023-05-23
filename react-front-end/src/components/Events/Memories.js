5047b7df0449b139ec076693f955bec902ef884c
Rain Zhu <rain1995zhu@gmail.com>
2023-05-23 03:14:46 -0700

    add get request by fundraiser id to events api

--- react-front-end/src/components/Events/Memories.js
+++ react-front-end/src/components/Events/Memories.js
@@ -1,29 +1,83 @@
-import React, { useContext, useState } from "react";
-import './Events.scss';
-import EventsInfo from './EventsInfo';
-import EventGuestList from "./EventGuestList";
-import axios from 'axios';
-import useEventsData from "../../hooks/useEventsData";
-import { getEventGuests, getEventInfo } from "../../helpers/event_selectors";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock, faCompactDisc, faEnvelope, faLayerGroup, faMagnifyingGlass, faMicrophoneLines } from "@fortawesome/free-solid-svg-icons";
+import Events from "./Events";
+import './Events.scss';
 import { useParams } from "react-router";
-import { friendContext } from 'providers/FriendProvider';
-import GuestFundraiser from "./GuestFundraiser";
-import HostFundraiser from "./HostFundraiser";
-
+import MemoriesListItem from "./MemoriesListItem";
 
 export default function Memories(props) {
 
+  const [donations, setDonations] = useState([]);
+  const [event, setEvent] = useState([]);
+
+  // change the tab
+  const [list, setList] = useState("allEvent");
+  function handleListClick(section) {
+    setList(section);
+  }
+
+  const params = useParams();
+
+  const fundraiser_id = params.id;
+
+  useEffect(() => {
+
+    axios.get(`/api/fundraiser-user/fundraiserinfo/${fundraiser_id}`)
+      .then((res) => {
+        console.log(res.data);
+        setDonations(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+  
+  useEffect(() => {
+
+    axios.get(`/api/fundraiser-user/fundraiserinfo/${fundraiser_id}`)
+      .then((res) => {
+        console.log(res.data);
+        setDonations(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+
+  const MessageProps = donations.map((donation) => {
+
+    const name = donation.first_name + " " + donation.last_name;
+
+    return (
+        <MemoriesListItem
+          key={donation.id}
+          host={props.user}
+          user={donation.user_id}
+          message={donation.message}
+          name={name}
+          date={donation.time}
+          photo={donation.photo}
+          amount={donation.amount}
+          anonymous={donation.payment_anonymous}
+        />
+    );
+  });
+
   return (
-    <main className="event-layout">
+    <>
+      <div className="eventListButtons display-flex">
+
+        <button className={`background-primary-color flex-one upcoming ${list === 'upcoming' ? '--selected' : ''}`}>
+          Memories for {event_name} on {event_date}.
+        </button>
+      </div>
+      <ul>{MessageProps}</ul>
 
       <section className="event-wall __card box-shadow border-radius20 background-box-color user-detail">
-        <Link to='/groupchat'>
-          <button onClick={""} className="background-point-color btn-style">Go Back to Event Page</button>
+        <Link to={`/events/${event_id}`}>
+          <button className="background-point-color btn-style">Go Back to Event Page</button>
         </Link>
       </section>
-
-
-    </main>
+    </>
   );
 }
